68ab33822c12b9dc9f4d6cfd5ffaae18a04c0ddd
ml2585 <ml2585@cornell.edu>
2019-09-15 21:22:34 -0400

    implement functions and travis test

--- sim/tut3_pymtl/regincr/RegIncr_extra_test.py
+++ sim/tut3_pymtl/regincr/RegIncr_extra_test.py
@@ -40,3 +40,29 @@ def test_large( dump_vcd ):
 # for random testing.
 # ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 
+def test_overflow( dump_vcd ):
+  run_test_vector_sim( RegIncr(), [
+    ('in_   out*'),
+    [ 0x00, '?'  ],
+    [ 0x03, 0x01 ],
+    [ 0x06, 0x04 ],
+    [ 0x00, 0x07 ],
+  ], dump_vcd )
+
+#-------------------------------------------------------------------------
+# test_random
+#-------------------------------------------------------------------------
+
+import random
+
+def test_random( dump_vcd ):
+
+  test_vector_table = [( 'in_', 'out*' )]
+  last_result = '?'
+  for i in xrange(20):
+    rand_value = Bits( 8, random.randint(0,0xff) )
+    test_vector_table.append( [ rand_value, last_result ] )
+    last_result = Bits( 8, rand_value + 1 )
+
+  run_test_vector_sim( RegIncr(), test_vector_table, dump_vcd )
+
